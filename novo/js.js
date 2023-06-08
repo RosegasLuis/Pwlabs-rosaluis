019ad296fa890ee941069a7df024972240288dc7
rosaluis <adalrosa1996@icloud.com>
2023-06-08 02:52:04 +0100

    atualizar

--- novo/js.js
+++ novo/js.js
@@ -1,10 +1,8 @@
 function marcarLinkClicado(elemento) {
-    // Remove a classe 'ativo' de todos os links
     var links = document.querySelectorAll('a');
     for (var i = 0; i < links.length; i++) {
         links[i].classList.remove('ativo');
     }
-
-    // Adiciona a classe 'ativo' ao link clicado
+    
     elemento.classList.add('ativo');
 }
